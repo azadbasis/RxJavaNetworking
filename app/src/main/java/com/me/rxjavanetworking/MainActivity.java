5b55bddfe6b2310b123c591f0203a853578e6051
Doodle <azharul@doodleinc.net>
2020-12-28 12:05:17 -0600

    Observables and Observers

--- app/src/main/java/com/me/rxjavanetworking/MainActivity.java
+++ app/src/main/java/com/me/rxjavanetworking/MainActivity.java
@@ -1,117 +1,58 @@
 package com.me.rxjavanetworking;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 
-import org.reactivestreams.Subscription;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
+import com.me.rxjavanetworking.models.Task;
+import com.me.rxjavanetworking.util.DataSource;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private GitHubRepoAdapter adapter = new GitHubRepoAdapter();
-    public final CompositeDisposable compositeDisposable = new CompositeDisposable();
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RecyclerView listView = findViewById(R.id.list_view_repos);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        listView.setLayoutManager(linearLayoutManager);
-        listView.setAdapter(adapter);
+        Observable<Task> taskObservable = Observable // create a new Observable object
+                .fromIterable(DataSource.createTasksList()) // apply 'fromIterable' operator
+                .subscribeOn(Schedulers.io()) // designate worker thread (background)
+                .observeOn(AndroidSchedulers.mainThread()); // designate observer thread (main thread)
 
-        final EditText editTextUsername = (EditText) findViewById(R.id.edit_text_username);
-        final Button buttonSearch = (Button) findViewById(R.id.button_search);
-        buttonSearch.setOnClickListener(new View.OnClickListener() {
+        taskObservable.subscribe(new Observer<Task>() {
             @Override
-            public void onClick(View v) {
-                final String username = editTextUsername.getText().toString();
-                if (!TextUtils.isEmpty(username)) {
-                    getStarredRepos(username);
-                }
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Task task) { // run on main thread
+
+                Log.d(TAG, "onNext: : " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
             }
         });
-
-
-/**
- * RxJava for each item in a list make api request with Retrofit in Android
- */
-
-    /*    Observable.fromIterable(categoryItems)
-                .flatMap(new Function<CategoryItem, ObservableSource<Pair<String, ProductResponse>>>() {
-                    @Override
-                    public ObservableSource<Pair<String, ProductResponse>> apply(@NonNull CategoryItem categoryItem) throws Exception {
-                        return Observable.zip(
-                                Observable.just(categoryItem. getId()),
-                                networkManager.getApiServiceProductsRx().getProductsRx(categoryItem. getId()),
-                                new BiFunction<String, ProductResponse, Pair<String, ProductResponse>>() {
-                                    @Override
-                                    public Pair<String, ProductResponse> apply(@NonNull String id, @NonNull ProductResponse productResponse) throws Exception {
-                                        return new Pair<String, ProductResponse>(id, productResponse);
-                                    }
-                                });
-                    }
-                })
-                .toList()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());*/
-
     }
 
     @Override
     protected void onDestroy() {
-        if (compositeDisposable != null) {
-            compositeDisposable.clear();
-        }
         super.onDestroy();
     }
 
-    private void getStarredRepos(String username) {
-        GitHubClient.getInstance()
-                .getStarredRepos(username)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<GitHubRepo>>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-                        compositeDisposable.add(d);
-                    }
-
-                    @Override
-                    public void onNext(@NonNull List<GitHubRepo> gitHubRepos) {
-                        adapter.setGitHubRepos(gitHubRepos);
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
-
-
-    }
 }
